d222e720cfe304b95c0a53ead62b0aabee9c057b
shabbir <shabbirhossain660@gmail.com>
2024-12-10 15:53:22 +0600

    states complete

--- app/aboutUs/page.jsx
+++ app/aboutUs/page.jsx
@@ -1,4 +1,5 @@
 import Happy from '@/components/AboutUs/Happy';
+import Stats from '@/components/AboutUs/States';
 import About from '@/components/Home/About';
 import React from 'react';
 
@@ -7,6 +8,7 @@ const about = () => {
         <div className='mt-[100px]'>
             <About/>
             <Happy/>
+            <Stats/>
         </div>
     );
 };
---
+++ components/AboutUs/States.jsx
@@ -0,0 +1,79 @@
+"use client"
+
+import CountUp from "react-countup";
+import { AiOutlineGlobal } from "react-icons/ai";
+import { PiHandshake } from "react-icons/pi";
+import { FcCustomerSupport } from "react-icons/fc";
+// import { LuChartNoAxesCombined } from "react-icons/lu";
+import { GiChart } from "react-icons/gi";
+
+
+
+const Stats = () => {
+    const stats = [
+        {
+            number: 12,
+            title: "Countries we served",
+            img: <AiOutlineGlobal />
+
+        },
+        {
+            number: 75,
+            title: "Happy Clients",
+            img: <PiHandshake />
+
+        },
+        {
+            number: 15,
+            title: "Experts",
+            img: <FcCustomerSupport />
+
+        },
+        {
+            number: 200,
+            title: "Project Completed",
+            img: <GiChart />
+
+
+        },
+    ]
+    return (
+        <section className="pt-4 pb-12 xl:pb-0 xl:pt-0 my-[100px]">
+            {/* <div className="container mx-auto">
+                <div className="flex flex-wrap justify-center items-center gap-6   ">
+                    {stats.map((item, index)=>
+                     <div key={index} className="border-[1px] rounded-2xl md:w-[350px] md:h-[288px] gap-4 items-center justify-center mx-auto">
+                        <CountUp
+                        end={item.number}
+                        duration={5}
+                        delay={2}
+                        className="text-4xl xl:text-6xl font-bold flex justify-center items-center mx-auto "
+                        />
+                        <p className="flex justify-center items-center mx-auto">{item.title}</p>
+                     </div>
+                    )}
+                </div>
+            </div> */}
+            <div className="flex flex-wrap justify-center items-center gap-6">
+                {stats.map((item, index) => (
+                    <div
+                        key={index}
+                        className="border-[1px] rounded-2xl md:w-[350px] md:h-[288px] flex flex-col items-center justify-center shadow-xl hover:bg-[#2EA2F4] hover:text-white"
+                    >
+                        <h1 className="text-[46px] text-[#2EA2F4] hover:text-white">{item.img} </h1>
+                        <CountUp
+                            end={item.number}
+                            duration={5}
+                            delay={2}
+                            suffix="+"
+                            className="text-[36px]  font-bold"
+                        />
+                        <p className="text-center font-light">{item.title}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Stats;
