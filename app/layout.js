b385a68e7efc7799a69dc2a8b81e7b9cb024dd7c
shabbir <shabbirhossain660@gmail.com>
2024-12-11 15:10:20 +0600

    google tag manager added

--- app/layout.js
+++ app/layout.js
@@ -29,10 +29,10 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MHWVQQ34');
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MHWVQQ34');
             `,
           }}
         />
@@ -44,7 +44,7 @@ export default function RootLayout({ children }) {
             src="https://www.googletagmanager.com/ns.html?id=GTM-MHWVQQ34"
             height="0"
             width="0"
-            style={{ display: "none", visibility: "hidden" }}
+            style="display:none;visibility:hidden"
           ></iframe>
         </noscript>
         <Header />
